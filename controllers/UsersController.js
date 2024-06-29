2bd537637f3cdcc73a52cf8644074d3a332597e3
mojtabababiker <mojmohammad98@gmail.com>
2024-06-29 09:13:28 +0200

    edit UserController, use async await instead of callbacks

--- controllers/UsersController.js
+++ controllers/UsersController.js
@@ -3,18 +3,17 @@
  */
 import { dbClient } from '../utils/utils';
 
-export default function postNew(req, res) {
-  /**
-    * api add new user endpoint
-    * @param {Express.request} req - The request object
-    * @param {Express.response} res - The response object
-    * @return object with new user email and id
-  */
+/**
+  * api add new user endpoint
+  * @param {Express.request} req - The request object
+  * @param {Express.response} res - The response object
+  * @return object with new user email and id
+*/
+export default async function postNew(req, res) {
   if (!dbClient.isAlive()) {
     res.status(500).json({ message: 'Can not connect to database' });
     return;
   }
-  // console.log(req.body);
   const { email, password } = req.body;
   if (email === undefined) {
     res.status(400).json({ error: 'Missing email' });
@@ -25,10 +24,11 @@ export default function postNew(req, res) {
     res.status(400).json({ error: 'Missing password' });
     return;
   }
-  dbClient.addUser(email, password)
-    .then((result) => {
-      res.status(201).json(result);
-    }).catch((err) => {
-      res.status(400).json({ error: err.message });
-    });
+
+  try {
+    const result = await dbClient.addUser(email, password);
+    res.status(201).json(result);
+  } catch (err) {
+    res.status(400).json({ error: err.message });
+  }
 }
