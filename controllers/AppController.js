1539525f9be30da3c1df7c53eaa161e1d3df9137
mojtabababiker <mojmohammad98@gmail.com>
2024-06-29 09:20:20 +0200

    Edit AppController, use async await instead of callbacks

--- controllers/AppController.js
+++ controllers/AppController.js
@@ -5,43 +5,30 @@
 // import { redisClient } from '../utils/redis';
 import { dbClient, redisClient } from '../utils/utils';
 
+/**
+ * api get status endpoint
+ * @param {Express.request} req - The request object
+ * @param {Express.response} res - The response object
+ * @returns {Response} json object indicates the status of the redis and db connection
+ */
 export function getStatus(req, res) {
-  /**
-   * api get status endpoint
-   * @param {Express.request} req - The request object
-   * @param {Express.response} res - The response object
-   * @returns {Response} json object indicates the status of the redis and db connection
-   */
-
   const status = {
-    db: dbClient.isAlive(),
     redis: redisClient.isAlive(),
+    db: dbClient.isAlive(),
   };
   res.json(status);
 }
 
-export function getStats(req, res) {
-  /**
-   * api get statistics about the db collection
-   * @param {Express.request} req - The request object
-   * @param {Express.response} res - The response object
-   * @returns {Response} json object indicates the stats of the db collection
-   */
+/**
+ * api get statistics about the db collection
+ * @param {Express.request} req - The request object
+ * @param {Express.response} res - The response object
+ * @returns {Response} json object indicates the stats of the db collection
+ */
+export async function getStats(req, res) {
   const stats = {
-    users: 0,
-    files: 0,
+    users: await dbClient.nbUsers(),
+    files: await dbClient.nbFiles(),
   };
-  dbClient.nbUsers()
-    .then((value) => {
-      stats.users = value;
-    })
-    .then(() => {
-      dbClient.nbFiles()
-        .then((value) => {
-          stats.files = value;
-        });
-    })
-    .finally(() => {
-      res.json(stats);
-    });
+  res.json(stats);
 }
