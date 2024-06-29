3e9d8ea4c5e3c5f2f792ea7302963952bc35e67e
mojtabababiker <mojmohammad98@gmail.com>
2024-06-29 09:05:55 +0200

    edit server module, fix typo

--- server.js
+++ server.js
@@ -5,12 +5,11 @@ import signApp from './routes/index';
 
 const express = require('express');
 
-// console.log(express);
-// process.exit();
 const app = express();
 app.use(express.json());
 
 // sign the app to the route blueprint
 signApp(app);
 
-app.listen(5000);
+const PORT = process.env.PORT || 5000;
+app.listen(PORT);
